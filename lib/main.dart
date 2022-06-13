25b99f37d98aeec4179623abe0146f94a1b5e17b
Lesetsam <48183845+Lesetsam@users.noreply.github.com>
2022-06-14 00:25:03 +0200

    Init Created signin/singup screen

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:amazon_clone/constant/global_variables.dart';
+import 'package:amazon_clone/route.dart';
+import 'package:flutter/material.dart';
+
+import 'features/auth/screens/auth_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        //primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GlobalVariable.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariable.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          )
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+    );
+  }
+}
+
+
+
